2e6f4d42a23fe9b4a4fd02cd6ba771cb89a5f250
aLearningLad <mocmanca@gmail.com>
2024-10-06 18:22:24 +0200

    additions to dependency injection container

--- obj/Debug/net8.0/dotnetapitut.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetapitut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetapitut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d674b683be72f99ea2d7c7610b284052daef768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597c720e4eba51a7b335eb0b2975208778d258ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetapitut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetapitut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
